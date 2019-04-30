06bb428c33b4bc86b4b794fd46f94027c6c343b1
Ryan Bravo <rrbravo2@gmail.com>
2019-04-30 06:52:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 function dwarfRollCall(dwarves) {
-  var newStr = "";
+var newStr = "";
   for (var i = 0; i < dwarves.length; i++) {
     if (i === dwarves.length +1) {
      newStr +=(i+1 + ". " + dwarves[i]);
@@ -12,7 +12,6 @@ function dwarfRollCall(dwarves) {
 }
 
 function summonCaptainPlanet(planeteerCalls){
-
 var result = [];
 planeteerCalls.forEach(function(ele) {
   ele = ele.toUpperCase() + "!";
